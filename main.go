b6e626a40f5db689d549d52665ccdef6c7cb4e4a
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2026-02-11 07:59:19 +0530

    main go file added

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello from Go + Docker")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	log.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
